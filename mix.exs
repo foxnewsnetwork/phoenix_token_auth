1a3c61612578fe7488c86295238e7a03a4ac9764
Thomas Chen <foxnewsnetwork@gmail.com>
2016-03-07 12:47:24 -0800

    fuck it, we will do it live

--- mix.exs
+++ mix.exs
@@ -56,10 +56,10 @@ defmodule PhoenixTokenAuth.Mixfile do
         {:comeonin, "~> 2.0.0"},
         {:postgrex, ">= 0.6.0"},
         {:joken, "~> 0.13.1"},
-        {:poison, "~> 1.4.0"},
+        {:poison, "~> 1.4"},
         {:secure_random, "~> 0.1.0"},
         {:mailgun, "~> 0.1.1"},
-        {:timex, "~> 0.19"},
+        {:timex, ">= 0.19.0"},
         # DEV
         {:earmark, "~> 0.1.0", only: :dev},
         {:ex_doc, "~> 0.7.0", only: :dev},
